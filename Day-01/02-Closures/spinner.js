79b67f438cca13a07c6e647b640eb72230c42d8c
tkmagesh <tkmagesh77@gmail.com>
2016-01-23 14:11:55 +0530

    Closures - Spinner assignment

---
+++ Day-01/02-Closures/spinner.js
@@ -0,0 +1,17 @@
+//Create an object and assign it to a variable 'spinner'
+
+var spinner = // object creation logic goes here
+
+//the object is expected to exhibit the following behavior
+
+spinner.up() // => 1
+spinner.up() // => 2
+spinner.up() // => 3
+spinner.up() // => 4
+
+spinner.down() // => 3
+spinner.down() // => 2
+spinner.down() // => 1
+spinner.down() // => 0
+spinner.down() // => -1
+
