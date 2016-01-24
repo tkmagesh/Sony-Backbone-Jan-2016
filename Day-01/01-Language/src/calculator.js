5ce21449f3d8af391b8fcc6dca5bd54875f562b2
tkmagesh <tkmagesh77@gmail.com>
2016-01-24 14:20:01 +0530

    Bug Tracker - Seed

--- Day-01/01-Language/src/calculator.js
+++ Day-01/01-Language/src/calculator.js
@@ -4,5 +4,5 @@ function add(x,y){
         if (Array.isArray(n)) return add.apply(this, n);
         return isNaN(n) ? 0 : parseInt(n,10);
     }
-    return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + add([].slice.call(arguments,1));
+    return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + add(Array.prototype.slice.call(arguments,1));
 }
--- Day-01/03-FunctionalProgramming/products.js
+++ Day-01/03-FunctionalProgramming/products.js
@@ -1,9 +1,9 @@
 var products = [
-    {id : 3, name : "Pen", cost : 20, units : 20, category : 1},
-    {id : 7, name : "Hen", cost : 100, units : 10, category : 2},
-    {id : 5, name : "Den", cost : 200, units : 30, category : 1},
-    {id : 4, name : "Ten", cost : 10, units : 10, category : 2},
-    {id : 6, name : "Zen", cost : 1000, units : 1, category : 1}
+    {id : 3, name : "Pen", cost : 20, units : 20, category : 10},
+    {id : 7, name : "Hen", cost : 100, units : 10, category : 20},
+    {id : 5, name : "Den", cost : 200, units : 30, category : 10},
+    {id : 4, name : "Ten", cost : 10, units : 10, category : 20},
+    {id : 6, name : "Zen", cost : 1000, units : 1, category : 10}
 ];
 
 /*
@@ -164,7 +164,8 @@ describe("Any", function(){
         });
         console.log(hasCostlyProducts);
     });
-})
+});
+
 describe("All", function(){
     function all(list, criteriaFn){
         for(var i=0; i<list.length; i++)
@@ -177,4 +178,102 @@ describe("All", function(){
         });
         console.log(areAllProductsCostly);
     });
+});
+
+describe("Min", function(){
+    function min(list, valueSelector){
+        var result = Number.MAX_VALUE;
+        for(var i=0; i<list.length; i++){
+            var value = valueSelector(list[i]);
+            if (value < result)
+                result = value;
+        }
+        return result;
+    }
+    console.log("Min cost = ", min(products, function(p){ return p.cost; }));
+    console.log("Min units = ", min(products, function(p){ return p.units; }));
 })
+
+describe("Aggregate", function(){
+
+    function aggregate(list, aggregator, start){
+        var result = start;
+        for(var i=0; i<list.length; i++)
+            result = aggregator(result, list[i]);
+        return result;
+    }
+
+    console.log("Sum of units = ", aggregate(products, function(result, product){
+        return result + product.units;
+    },0));
+
+    console.log("Min of units = ", aggregate(products, function(result, product){
+        return result < product.units ? result : product.units;
+    }, Number.MAX_VALUE));
+
+    console.log("Max of cost = ", aggregate(products, function(result, product){
+        return result > product.cost ? result : product.cost;
+    }, Number.MIN_VALUE));
+
+    function averageCost (){
+        var sumdata = aggregate(products, function(result, product){
+            return {total : result.total + product.cost, count : result.count + 1};
+        }, {total : 0, count : 0});
+        console.log(sumdata);
+        return sumdata.total / sumdata.count;
+    }
+    console.log("Average cost = ", averageCost());
+});
+
+describe("GroupBy", function(){
+    function groupBy(list, keySelector){
+        var result = {};
+        for(var i=0; i<list.length; i++){
+            var key = keySelector(list[i]);
+            if (typeof result[key] === 'undefined')
+                result[key] = [];
+            result[key].push(list[i]);
+        }
+        return result;
+    }
+    function printGroup(list){
+        for(var key in list){
+            describe("Key - " + key, function(){
+                console.table(list[key]);
+            });
+        }
+    }
+    describe("Products by category", function(){
+        var productsByCategory = groupBy(products, function(product){
+            return product.category;
+        });
+        printGroup(productsByCategory);
+    })
+    describe("Products by cost", function(){
+        var productsByCost = groupBy(products, function(product){
+            return product.cost <= 100 ? "affordable" : "costly";
+        });
+        printGroup(productsByCost);
+    })
+});
+
+describe("transform", function(){
+    function transform(list, transformFn){
+        var result = [];
+        for(var i=0; i<list.length; i++)
+            result.push(transformFn(list[i]));
+        return result;
+    }
+    describe("products after a 10% discount", function(){
+        var productsWith10PercentDiscount = transform(products, function(product){
+            return {
+                id : product.id,
+                name: product.name,
+                units : product.units,
+                cost : product.cost * 0.9,
+                category : product.category
+            };
+        });
+        console.table(productsWith10PercentDiscount);
+    });
+});
